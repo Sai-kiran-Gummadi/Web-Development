98b426f897d19bf8cc5796db6cdbcba76e0c2219
Sai-kiran-Gummadi <gummadisaikiran143@gmail.com>
2023-03-19 12:35:08 +0530

    doms2

--- DOM/s2.js
+++ DOM/s2.js
@@ -2,3 +2,6 @@ const items = document.querySelector('#items');
 items.children[2].style.background = 'green';
 
 items.style.fontWeight = 'bold';
+const li = document.getElementsByTagName('li');
+li[4].style.backgroundColor = 'green';
+console.log(li)
